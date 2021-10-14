1b9529bbe711b09a304810d4a31fa2a516d2bda3
VendelaAJ <vendela.swenson@hotmail.se>
2021-10-14 14:48:38 +0200

    added-last-jss-css

--- script.js
+++ script.js
@@ -1,26 +1,28 @@
-"use strict";
+'use strict';
 
-const checkInPass = document.querySelector(".check-in");
-const fastTrack = document.querySelector(".fast-track");
-const container = document.querySelector("#check-ins");
-const removeFirstInLine = document.querySelector(".remove-first");
-let myOrdered = document.querySelector(".ordered-list");
-let listItems = "";
+const checkInPass = document.querySelector('.check-in');
+const fastTrack = document.querySelector('.fast-track');
+const container = document.querySelector('#check-ins');
+const removeFirstInLine = document.querySelector('.remove-first');
+let myOrdered = document.querySelector('.ordered-list');
+let listItems = '';
 let showInput = false;
 
 const bookings = [];
 
-const addInput = (fastTrack) => {
+const addInput = fastTrack => {
   showInput = true;
-  const nameInput = document.createElement("input");
-  const addBtn = document.createElement("button");
-  nameInput.type = "text";
-  nameInput.placeholder = "Enter passengers name here";
+  const nameInput = document.createElement('input');
+  const addBtn = document.createElement('button');
+  nameInput.className = 'input';
+  addBtn.className = 'button';
+  nameInput.type = 'text';
+  nameInput.placeholder = 'Enter passengers name here';
   container.appendChild(nameInput);
   container.appendChild(addBtn);
-  addBtn.innerHTML = "Add";
+  addBtn.innerHTML = 'Add';
 
-  addBtn.addEventListener("click", () => {
+  addBtn.addEventListener('click', () => {
     alert(`${nameInput.value} is now in line`);
     fastTrack
       ? bookings.unshift(nameInput.value)
@@ -32,28 +34,28 @@ const addInput = (fastTrack) => {
   });
 };
 
-checkInPass.addEventListener("click", () => {
+checkInPass.addEventListener('click', () => {
   if (!showInput) addInput();
 });
 
-fastTrack.addEventListener("click", () => {
+fastTrack.addEventListener('click', () => {
   if (!showInput) addInput(fastTrack);
 });
 
-removeFirstInLine.addEventListener("click", () => {
+removeFirstInLine.addEventListener('click', () => {
   bookings.shift();
   updateList();
 
   if (bookings.length < 1)
-    alert("There’s currently no people standing in line");
+    alert('There’s currently no people standing in line');
 });
 
 const updateList = () => {
   while (myOrdered.firstChild) {
     myOrdered.removeChild(myOrdered.firstChild);
   }
-  bookings.forEach((i) => {
-    const listItem = document.createElement("li");
+  bookings.forEach(i => {
+    const listItem = document.createElement('li');
     listItem.innerText = i;
     myOrdered.appendChild(listItem);
   });
